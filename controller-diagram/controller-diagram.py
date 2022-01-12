75e41926fb3bedad8c05afea4a3d7225685bf46d
Felix Book <64723297+fbook98@users.noreply.github.com>
2022-01-12 15:52:20 +0100

    Create controller-diagram.py

---
+++ controller-diagram/controller-diagram.py
@@ -0,0 +1,53 @@
+from pylatex import Document, TikZ
+from tkinter import filedialog
+from tkinter import *
+import os
+
+
+class ControllerDiagram:
+
+    def __init__(self, data_type=('pdf',)):
+        self._data_type = tuple(data_type)
+        self._pdf_name = None
+        self._clean_tex = 'tex' not in self._data_type
+        self._components = []
+
+    def append(self, component):
+        if isinstance(component, (list, tuple)):
+            self._components.extend(component)
+        else:
+            self._components.append(component)
+
+    def build(self):
+        doc = Document(page_numbers=False, geometry_options={'landscape': True, 'includeheadfoot': False})
+        with doc.create(TikZ()) as pic:
+            for component in self._components:
+                component.build(pic)
+
+        for filename in self._get_filename():
+            name, data_type = filename.split('.', 1)
+            if data_type == 'pdf':
+                doc.generate_pdf(name, compiler='pdflatex', clean_tex=self._clean_tex)
+                self._pdf_name = filename
+            elif data_type == 'tex':
+                doc.generate_tex(name)
+
+    def show(self):
+        if self._pdf_name is not None:
+            os.system(self._pdf_name)
+
+    def _get_filename(self):
+        win = Tk()
+        win.withdraw()
+        for data_type in self._data_type:
+            if 'pdf' == data_type:
+                filetypes = (('Portable Document Format (*.pdf)', '*.pdf'), ('All Files', '*.*'))
+                yield filedialog.asksaveasfilename(initialdir='/', title='Save as', filetypes=filetypes,
+                                                   defaultextension='.pdf')
+            elif 'tex' == data_type:
+                filetypes = (('TeX Document (*.tex)', '*.tex'), ('All Files', '*.*'))
+                yield filedialog.asksaveasfilename(initialdir='/', title='Save as', filetypes=filetypes,
+                                                   defaultextension='.tex')
+            else:
+                raise ValueError(
+                    f'The file type {data_type} is not supported. Use the Portable Document Format (pdf) or Tex Document (tex) file type.')
