3b0ed0b9e0476fad22c2d7127975b81f3762e780
Felix Book <felix.book98@gmail.com>
2022-03-01 10:40:44 +0100

    Font not italic

--- control_block_diagram/predefined_components/electric_components/coordinate_transformations/dq_to_alpha_beta_transformation.py
+++ control_block_diagram/predefined_components/electric_components/coordinate_transformations/dq_to_alpha_beta_transformation.py
@@ -6,5 +6,5 @@ class DqToAlphaBetaTransformation(CoordinateTransformation):
     def __init__(self, position, size: float = 1.5, input: str = 'left', output: str = 'right',
                  additional_inputs: dict = dict(bottom=1), additional_outputs: dict = dict(top=1),
                  input_space: float = 0.6, output_space: float = 0.6, doc=None):
-        super().__init__(position, size, ['$dq$'], [r'$\alpha\beta$'], input, 2, output, 2, additional_inputs,
-                         additional_outputs, input_space, output_space, doc)
+        super().__init__(position, size, [r'$\mathrm{dq}$'], [r'$\mathrm{\alpha\beta}$'], input, 2, output, 2,
+                         additional_inputs, additional_outputs, input_space, output_space, doc)
