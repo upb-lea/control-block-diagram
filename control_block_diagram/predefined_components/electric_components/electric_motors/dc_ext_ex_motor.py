5b9ff446febc062e193f4a15c40b8b5b243f36f6
Felix Book <felix.book98@gmail.com>
2022-04-04 10:18:32 +0200

    bugfix new version number

--- control_block_diagram/predefined_components/electric_components/electric_motors/dc_ext_ex_motor.py
+++ control_block_diagram/predefined_components/electric_components/electric_motors/dc_ext_ex_motor.py
@@ -2,7 +2,7 @@ from .electric_motor import ElectricMotor
 
 
 class DcExtExMotor(ElectricMotor):
-    def __init__(self, position, size=1.5, input: (list, tuple) = ['left', 'right'], output: str = 'left',
+    def __init__(self, position, size=1.5, input: str = 'left', output: str = 'left',
                  orientation: str = 'bottom', input_space: float = 0.25):
 
         super().__init__(position, 'DC\nExt\nEx', size, input, 4, output, orientation, input_space, dict())
