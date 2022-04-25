cc6c5fc423b9d2a658b5108c63f64caba65215e2
Felix Book <felix.book98@gmail.com>
2022-04-25 12:30:57 +0200

    add inputs and outputs to custom blocks

--- control_block_diagram/components/blocks/custom_block/custom_block.py
+++ control_block_diagram/components/blocks/custom_block/custom_block.py
@@ -12,6 +12,66 @@ class CustomBlock(Block):
         """Returns the points of a custom block"""
         return self._points
 
+    @property
+    def input_left(self):
+        """Returns all inputs on the left side of a block as a list"""
+        return self._input_left
+
+    @input_left.setter
+    def input_left(self, inputs_left):
+        if all(isinstance(inp, Point) for inp in inputs_left):
+            self._input_left = inputs_left
+
+    @property
+    def input_top(self):
+        """Returns all inputs on the top side of a block as a list"""
+        return self._input_top
+
+    @input_top.setter
+    def input_top(self, inputs_top):
+        if all(isinstance(inp, Point) for inp in inputs_top):
+            self._input_top = inputs_top
+
+    @property
+    def input_right(self):
+        """Returns all inputs on the right side of a block as a list"""
+        return self._input_right
+
+    @input_right.setter
+    def input_right(self, inputs_right):
+        if all(isinstance(inp, Point) for inp in inputs_right):
+            self._input_right = inputs_right
+
+    @property
+    def input_bottom(self):
+        """Returns all inputs on the bottom side of a block as a list"""
+        return self._input_bottom
+
+    @input_bottom.setter
+    def input_bottom(self, inputs_bottom):
+        if all(isinstance(inp, Point) for inp in inputs_bottom):
+            self._input_bottom = inputs_bottom
+
+    @property
+    def output_left(self):
+        """Returns all outputs on the left side of a block as a list"""
+        return self._output_left
+
+    @property
+    def output_top(self):
+        """Returns all outputs on the top side of a block as a list"""
+        return self._output_top
+
+    @property
+    def output_right(self):
+        """Returns all outputs on the right side of a block as a list"""
+        return self._output_right
+
+    @property
+    def output_bottom(self):
+        """Returns all outputs on the bottom side of a block as a list"""
+        return self._output_bottom
+
     def __init__(self, points: [Point], text: (Text, str) = None, text_configuration: dict = dict(), level: int = 0,
                  *args, **kwargs):
         """
